2f0924de69d1b766f8dbb8c69555a5559100aa76
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-06-04 14:56:10 +0300

    add phase-1-first-class-functions

---
+++ index.js
@@ -0,0 +1,18 @@
+const { run } = require("mocha");
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("Named function called");
+  }
+  return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("Anonymous function called");
+  };
+}
